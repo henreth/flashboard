1c087dcfb646c860df2e9864fa996498ce4fd0bb
Henry Miller <mail.henrym+git@gmail.com>
2022-06-27 15:30:40 -0400

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Home from '../Home/Home.tsx';
+import './App.css';
+
+
+let blocksUrl = 'https://blocks.epheph.com/v1/blocks'
+let flashUrl = 'https://blocks.flashbots.net/v1/blocks' 
+let relayUrl = 'http://relay.epheph.com/v1/blocks'
+export default function App() {
+  let [testData, setTestData] = useState([])
+
+  useEffect(() => {
+    // axios.get(flashUrl)
+    axios.get(blocksUrl)
+      .then(r => setTestData(r.data.blocks))
+  }, [])
+
+  return (
+    <React.Fragment>
+      <Home
+        testData={testData}
+      />
+    </React.Fragment>
+  );
+}
+
+
